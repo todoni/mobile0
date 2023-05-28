df99c4aacb3e661892b4dc00878d9ea6f439ee53
sohan <sohan@student.42seoul.kr>
2023-05-28 17:48:46 +0900

    Feat: ex01

--- my_first_proj/App.tsx
+++ my_first_proj/App.tsx
@@ -1,12 +1,18 @@
 import { StyleSheet, Text, View, Button } from "react-native";
+import { useState } from "react";
 
 export default function App() {
+  const [text, setText] = useState("Simple Text");
+
   return (
     <View style={styles.container}>
-      <Text>Simple Text</Text>
+      <Text>{text}</Text>
       <Button
         onPress={() => {
           console.log("Button pressed");
+          text === "Simple Text"
+            ? setText("Hello World!")
+            : setText("Simple Text");
         }}
         title={"Button"}
       />
